8dcfe0a5e85af0503f97c5ba29901923bb0f31b3
made-it-out <mike.hallisy@gmail.com>
2021-05-05 15:20:32 -0400

    mongodb uri

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const adminRoutes = require('./routes/adminRoutes')
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 //connect to mongodb
 const dbURI = process.env.DB_URI
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://made-it-out:test12345@mycluster.2t33q.mongodb.net/barbershop', {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result)=>{
-    app.listen(PORT);
+    app.listen(port);
 })
 .catch((err) => console.log(err))
 
